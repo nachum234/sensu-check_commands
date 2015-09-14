d07fb1e2588e9facf9864c085f408432345f4246
nachum234 <nachum234@gmail.com>
2015-09-14 22:03:01 +0300

    First version of check-snmp_disk.rb

--- check-snmp-disk.rb
+++ check-snmp-disk.rb
@@ -1,20 +1,25 @@
 #!/usr/bin/env ruby
-# Check SNMP
+# Check SNMP Disk
 # ===
 #
-# This is a simple SNMP check script for Sensu, We need to supply details like
-# Server, port, SNMP community, and Limits
+# This is a simple SNMP check disk script for Sensu,
+# The tool get a mount point as a regex and search for it in the device description SNMP table
+# and return the utilization status based on warning and critical thresholds
 #
 #
 # Requires SNMP gem
 #
 # USAGE:
 #
-#   check-snmp -h host -C community -O oid -w warning -c critical
-#   check-snmp -h host -C community -O oid -m "(P|p)attern to match\.?"
+#   check-snmp -h host -C public -m /mnt -w 75 -c 85
 #
+#   if you want to search for specific device and not regex add a comma to the pattern:
+#   check-snmp -h host -C public -m /, 
 #
-#  Author Deepak Mohan Das   <deepakmdass88@gmail.com>
+#  To search for device description in your server or applicance run the following command:
+#  snmpwalk -v2c -c public host 1.3.6.1.2.1.25.2.3.1.3
+#
+#  Author Yossi Nachum   <nachum234@gmail.com>
 #
 # Released under the same terms as Sensu (the MIT license); see LICENSE
 # for details.
@@ -32,50 +37,77 @@ class CheckSNMP < Sensu::Plugin::Check::CLI
          short: '-C snmp community',
          default: 'public'
 
-  option :objectid,
-         short: '-O OID',
-         default: '1.3.6.1.4.1.2021.10.1.3.1'
+  option :mount_point,
+         short: '-m mount point',
+         default: '/'
+
+  option :ignoremnt,
+         short: '-i MNT[,MNT]',
+         description: 'Ignore mount point(s)',
+         proc: proc { |a| a.split(',') }
 
   option :warning,
          short: '-w warning',
-         default: '10'
+         proc: proc(&:to_i),
+         default: 80
 
   option :critical,
          short: '-c critical',
-         default: '20'
-
-  option :match,
-         short: '-m match',
-         description: 'Regex pattern to match against returned value'
+         proc: proc(&:to_i),
+         default: 90
 
   option :snmp_version,
          short: '-v version',
          description: 'SNMP version to use (SNMPv1, SNMPv2c (default))',
          default: 'SNMPv2c'
 
-  option :comparison,
-         short: '-o comparison operator',
-         description: 'Operator used to compare data with warning/critial values. Can be set to "le" (<=), "ge" (>=).',
-         default: 'ge'
-
   option :timeout,
          short: '-t timeout (seconds)',
          default: '1'
+  
+  def initialize
+    super
+    @crit_mnt = []
+    @warn_mnt = []
+  end
+
+  def usage_summary
+    (@crit_mnt + @warn_mnt).join(', ')
+  end
 
   def run
     base_oid = '1.3.6.1.2.1.25.2.3.1'
-    dev_desc = "#{base_oid}.3"
-    dev_unit = base_oid + '.4'
-    dev_size = base_oid + '.5'
-    dev_used = base_oid + '.6'
+    dev_desc_oid = base_oid + '.3'
+    dev_unit_oid = base_oid + '.4'
+    dev_size_oid = base_oid + '.5'
+    dev_used_oid = base_oid + '.6'
     begin
       manager = SNMP::Manager.new(host: "#{config[:host]}",
                                   community: "#{config[:community]}",
                                   version: config[:snmp_version].to_sym,
                                   timeout: config[:timeout].to_i)
-      manager.walk(["#{dev_desc}"]) do |index|
-        index.each_varbind do |vb|
-          puts vb.value
+      response = manager.get_bulk(0, 200, [dev_desc_oid])
+      dev_indexes = []
+      response.each_varbind do |var|
+        if var.value.to_s =~ /#{config[:mount_point]}/
+          dev_indexes.push(var.name[-1])
+        end
+      end
+      dev_indexes.each do |dev_index|
+        response = manager.get(["#{dev_desc_oid}.#{dev_index}", "#{dev_unit_oid}.#{dev_index}", "#{dev_size_oid}.#{dev_index}", "#{dev_used_oid}.#{dev_index}"])
+        list = response.varbind_list
+        until list.empty?
+          dev_desc = list.shift
+          dev_unit = list.shift
+          dev_size = list.shift
+          dev_used = list.shift
+          next if config[:ignoremnt] && config[:ignoremnt].include?(dev_desc.value.to_s.split(",")[0])
+          perc = dev_used.value.to_f / dev_size.value.to_f * 100
+          if perc > config[:critical]
+            @crit_mnt << "#{dev_desc.value.to_s} = #{perc.round(2)}%"
+          elsif perc > config[:warning]
+            @warn_mnt << "#{dev_desc.value.to_s} = #{perc.round(2)}%"
+          end
         end
       end
     rescue SNMP::RequestTimeout
@@ -83,26 +115,9 @@ class CheckSNMP < Sensu::Plugin::Check::CLI
     rescue => e
       unknown "An unknown error occured: #{e.inspect}"
     end
-    exit
-    operators = { 'le' => :<=, 'ge' => :>= }
-    symbol = operators[config[:comparison]]
-
-    response.each_varbind do |vb|
-      if config[:match]
-        if vb.value.to_s =~ /#{config[:match]}/
-          ok
-        else
-          critical "Value: #{vb.value} failed to match Pattern: #{config[:match]}"
-        end
-      else
-        critical 'Critical state detected' if "#{vb.value}".to_i.send(symbol, "#{config[:critical]}".to_i)
-        # #YELLOW
-        warning 'Warning state detected' if ("#{vb.value}".to_i.send(symbol, "#{config[:warning]}".to_i)) && !("#{vb.value}".to_i.send(symbol, "#{config[:critical]}".to_i)) # rubocop:disable LineLength
-        unless "#{vb.value}".to_i.send(symbol, "#{config[:warning]}".to_i)
-          ok 'All is well!'
-        end
-      end
-    end
+    critical usage_summary unless @crit_mnt.empty?
+    warning usage_summary unless @warn_mnt.empty?
+    ok "All disk usage under #{config[:warning]}%"
     manager.close
   end
 end
